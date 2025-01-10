4a0ef78bd489f3355db868a96ab86dd3f30767ff
AmtelICT <ukash.upwork@gmail.com>
2025-01-10 22:35:15 +0300

    post

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Herbs pharmacy KE</h1>
+  
+        <iframe
+        width="100%"
+        height="514"
+        src="https://www.youtube.com/embed/SvaA7I3EmJQ?si=tPX3IL07ep2NTN78"  // Replace with your YouTube video URL
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
